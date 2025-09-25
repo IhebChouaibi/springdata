b95a151b1e1893e76dda2af05192ed28847ca289
iheb <ihebch316@gmail.com>
2025-09-25 21:40:39 +0100

    1

--- src/main/java/com/example/tp1/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tp1.repositories;
-
-import com.example.tp1.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
